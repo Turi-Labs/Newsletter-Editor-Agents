80f691a6a81902bfa909cf5282785d0f922f7f51
theyashwanthsai <taddishetty34@gmail.com>
2025-12-08 19:54:24 +0530

    added changes to the env

--- main.py
+++ main.py
@@ -26,30 +26,30 @@ research_notes_path = os.path.join(today_dir, "research_notes.md")
 newsletter_path = os.path.join(today_dir, "newsletter.md")
 logger.info(f"Newsletter will be saved to: {newsletter_path}")
 
-write_hn_posts(today, 5, hn_posts_path)
+# write_hn_posts(today, 5, hn_posts_path)
 
-filter(hn_posts_path, filtered_posts)
+# filter(hn_posts_path, filtered_posts)
 
-# append all summaries to a txt file
-l = get_links(filtered_posts)
-logger.info("Links extracted successfully")
-c = get_link_content(l)
-logger.info("Content extracted from links")
-s = send_to_ai(c)
-logger.info("AI summaries generated")
+# # append all summaries to a txt file
+# l = get_links(filtered_posts)
+# logger.info("Links extracted successfully")
+# c = get_link_content(l)
+# logger.info("Content extracted from links")
+# s = send_to_ai(c)
+# logger.info("AI summaries generated")
 
-# Write the AI response to a text file
-logger.info("Started writing summaries to research_notes.md")
-with open(research_notes_path, 'w', encoding='utf-8') as file:
-    for i, summary in enumerate(s, 1):
-        file.write(f"Summary {i}:\n")
-        file.write(f"{summary}\n")
-        file.write("\n")
+# # Write the AI response to a text file
+# logger.info("Started writing summaries to research_notes.md")
+# with open(research_notes_path, 'w', encoding='utf-8') as file:
+#     for i, summary in enumerate(s, 1):
+#         file.write(f"Summary {i}:\n")
+#         file.write(f"{summary}\n")
+#         file.write("\n")
 
-content = ''
-with open(research_notes_path, 'r') as file:
-    content = file.read()
+# content = ''
+# with open(research_notes_path, 'r') as file:
+#     content = file.read()
 
-logger.info("Newsletter Generation started")
-craft_newsletter(content, newsletter_path)
-logger.info("Newsletter successfully generated and saved")
+# logger.info("Newsletter Generation started")
+# craft_newsletter(content, newsletter_path)
+# logger.info("Newsletter successfully generated and saved")
